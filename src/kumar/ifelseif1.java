2c10325ba144f2b26e2d828ed1801a4de240757f
kumarshivamj <cu15bcs1191@gmail.com>
2025-04-20 03:04:08 +0530

    updating ifelseif1.java

--- src/kumar/ifelseif1.java
+++ src/kumar/ifelseif1.java
@@ -1,10 +1,38 @@
 package kumar;
 
+import java.util.Scanner;
+
 public class ifelseif1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		public static void main(String[] args) {
+			System.out.println("Enter the number 1 : ");
+				Scanner sc = new Scanner(System.in);
+				int a = sc.nextInt();
+				System.out.println("Enter the number 2 : ");
+				int b = sc.nextInt();
+				System.out.println("Enter the number 3 : ");
+				int c = sc.nextInt();
+				System.out.println("Enter the number 4 : ");
+				int d = sc.nextInt();
+				
+				if(a>b && a>c && a>d) {
+					System.out.println(a  + " A is the largest number");
+				}
+				
+				else if(b>c && b>d) { 
+					System.out.println(b  + " B is largest number");
+			}
+				else if(c>d) {
+					System.out.println(c + " C is greater number");
+				}
+			
+			else {
+				System.out.println(d  + " D is greater");
+			}
+				
+		}
 
 	}
 
-}
+
