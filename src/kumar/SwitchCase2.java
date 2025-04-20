1d9322b7cb9eb3f63cb1c50414ee414cc6316e85
kumarshivamj <cu15bcs1191@gmail.com>
2025-04-20 16:37:51 +0530

    Find days of week using switch

---
+++ src/kumar/SwitchCase2.java
@@ -0,0 +1,106 @@
+package kumar;
+import java.util.Scanner;
+public class SwitchCase2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number 1 to 7 :  ");
+	int day = sc.nextInt();
+		String result = "";
+		switch(day)
+		{
+		case 1:
+			result = "Sunday";
+		 break;
+		case 2:
+			result = "Monday";
+		break;
+		case 3:
+			result = "Tuesday";
+		break;
+		case 4:
+			result = "Wednesday";
+		break;
+		case 5:
+			result = "Thrusday";
+		break;
+		case 6:
+			result = "Friday";
+		break;
+		case 7:
+		  result = "Saturday";
+		break;
+		default:
+			System.out.println("Invalid Option!!!!");
+		}
+		System.out.println("Day: " + result);
+		}
+	}
+
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
